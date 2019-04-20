ccc92902faaa482560f77c67d31b7f63bd10136f
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2019-04-20 14:35:40 -0600

    Add load and saving of wordlists files.

--- phos/wordlist.py
+++ phos/wordlist.py
@@ -1,18 +1,25 @@
 import numpy as np
 from sklearn.cluster import KMeans, MiniBatchKMeans
 
-from phos.features import ExtractorID, create_feature_extractor
+from phos.features import FeatureExtractorID, create_feature_extractor
+
+__all__ = ['WordlistGenerator', 'save_wordlist', 'load_wordlist']
 
 
 class WordlistGenerator:
 
     def __init__(self, images=[], *,
                  max_features_per_image=None, method_id=None):
+        self._method_id = method_id
         self._feature_extractor = create_feature_extractor(method_id)
         self._max_features_per_file = max_features_per_image
         self._descriptors = []
         for image in images:
             self.add_image(image)
+
+    @property
+    def method_id(self):
+        return self._feature_extractor.id
 
     def add_image(self, image):
         features = self._feature_extractor.extract(
@@ -25,7 +32,6 @@ class WordlistGenerator:
         import time
         t = time.time()
         self._pack_descriptors()
-        print(f'time = {time.time() - t}')
         if (max_features is not None and
                 self._descriptors[0].shape[0] > max_features):
             idx = np.random.choice(
@@ -46,3 +52,20 @@ class WordlistGenerator:
     def _pack_descriptors(self):
         if len(self._descriptors) > 2:
             self._descriptors = [np.vstack(self._descriptors)]
+
+
+def save_wordlist(file, words, method):
+    # import ipdb; ipdb.set_trace()
+    header = np.array(
+        [method, words.shape[1], words.shape[0], 0], dtype=np.uint16)
+    with open(file, 'wb') as f:
+        f.write(header.tobytes())
+        f.write(words.astype(np.float32).tobytes())
+
+
+def load_wordlist(file):
+    with open(file, 'rb') as f:
+        header = np.fromfile(f, dtype=np.uint16, count=4)
+        method = FeatureExtractorID(header[0])
+        words = np.fromfile(f, dtype=np.float32).reshape(header[2], header[1])
+        return method, words
